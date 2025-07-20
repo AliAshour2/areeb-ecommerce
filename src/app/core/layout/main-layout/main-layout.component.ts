ad8bf28b2b59c435b54410f261ac77c0bc9a1aaf
AliAshour2 <aliashour468@gmail.com>
2025-07-20 12:55:24 +0300

    feat(layout): integrate ToastComponent into MainLayout for notifications
    - Add app-toast component to main-layout.component.html for displaying messages
    - Inject ToastService in MainLayoutComponent to manage toast notifications
    - Update imports in MainLayoutComponent to include ToastComponent for modularity

--- src/app/core/layout/main-layout/main-layout.component.ts
+++ src/app/core/layout/main-layout/main-layout.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { HeaderComponent } from "../components/header/header.component";
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from "../components/sidebar/sidebar.component";
+import { ToastService } from '../../services/toast/toast.service';
+import { ToastComponent } from '../../../shared/components/toast/toast.component';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [HeaderComponent, RouterOutlet, SidebarComponent , RouterOutlet],
+  imports: [HeaderComponent, RouterOutlet, SidebarComponent , RouterOutlet , ToastComponent],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.css',
   standalone: true
 })
 export class MainLayoutComponent {
+  private toastService = inject(ToastService);
 
+  get toast(){
+    return this.toastService.toast;
+  }
 }
